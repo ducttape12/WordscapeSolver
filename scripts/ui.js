6c74ffe56118cfa79993b604d40b97acdc45125b
ducttape12 <ducttape12@yahoo.com>
2017-09-22 17:09:00 +0000

    Form validation

--- scripts/ui.js
+++ scripts/ui.js
@@ -1,3 +1,52 @@
 var ui = (function() {
     'use strict';
+    
+    var cleanText = function(text) {
+        return text.trim().replace(/ /g, '');
+    };
+    
+    var cleanPossibleLetters = function() {
+        return cleanText($('#possibleLetters').val());
+    };
+    
+    var cleanPattern = function() {
+        return cleanText($('#pattern').val());
+    };
+    
+    var formValid = function() {
+        var possibleLetters = cleanPossibleLetters();
+        var pattern = cleanPattern();
+        var valid = true;
+
+        if (possibleLetters.length === 0) {
+            $('#invalidPossibleLetters').removeClass('d-none');
+        }
+        else {
+            $('#invalidPossibleLetters').addClass('d-none');
+            valid = false;
+        }
+
+        if (pattern.length === 0) {
+            $('#invalidPattern').removeClass('d-none');
+        }
+        else {
+            $('#invalidPattern').addClass('d-none');
+            valid = false;
+        }
+
+        return valid;
+    };
+
+    var initialize = function() {
+        $('#submit').click(function() {
+            if(formValid()) {
+                
+            }
+        });
+    };
+
+    return {
+        initialize: initialize
+    };
+
 })();
