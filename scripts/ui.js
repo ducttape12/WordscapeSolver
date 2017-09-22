c19da67f05d60c657075709cdc3b6e1638feadf8
ducttape12 <ducttape12@yahoo.com>
2017-09-22 18:13:52 +0000

    Reset form working, results displayed correctly

--- scripts/ui.js
+++ scripts/ui.js
@@ -1,23 +1,23 @@
 var ui = (function() {
     'use strict';
-    
+
     var cleanText = function(text) {
         return text.trim().replace(/ /g, '');
     };
-    
+
     var cleanPossibleLetters = function() {
         return cleanText($('#possibleLetters').val());
     };
-    
+
     var cleanPattern = function() {
         return cleanText($('#pattern').val());
     };
-    
+
     var formValid = function() {
         var possibleLetters = cleanPossibleLetters();
         var pattern = cleanPattern();
         var valid = true;
-        
+
         $('#invalidPossibleLetters').addClass('d-none');
         $('#invalidPattern').addClass('d-none');
 
@@ -25,7 +25,7 @@ var ui = (function() {
             $('#invalidPossibleLetters').removeClass('d-none');
             valid = false;
         }
-        
+
         if (pattern.length === 0 || !pattern.includes('?')) {
             $('#invalidPattern').removeClass('d-none');
             valid = false;
@@ -34,21 +34,45 @@ var ui = (function() {
         return valid;
     };
 
+    var findMatches = function() {
+        if (formValid()) {
+            var matches = solver.findWords(cleanPattern(), cleanPossibleLetters());
+
+            var solutions = $('#solutions');
+
+            solutions.empty();
+            $('#solutionGroup').removeClass('d-none');
+
+            for (var index = 0; index < matches.length; index++) {
+                solutions.append('<li class="list-group-item">' + matches[index] + '</li>')
+            }
+        }
+    };
+    
+    var reset = function() {
+        $('#invalidPossibleLetters').addClass('d-none');
+        $('#invalidPattern').addClass('d-none');
+        $('#solutionGroup').addClass('d-none');
+        
+        $('#pattern').focus();
+    };
+
+
     var initialize = function() {
-        $('#submit').click(function() {
-            if(formValid()) {
-                var matches = solver.findWords(cleanPattern(), cleanPossibleLetters());
-                
-                var solutions = $('#solutions');
-                
-                solutions.empty();
-                $('#solutionGroup').removeClass('d-none');
-                
-                for(var index = 0; index < matches.length; index++) {
-                    solutions.append('<li class="list-group-item">'+ matches[index] + '</li>')
-                }
+        $('#submit').click(findMatches);
+        $('#possibleLetters').on('keyup', function(e) {
+            if(e.keyCode === 13) {
+                findMatches();
             }
         });
+        $('#pattern').on('keyup', function(e) {
+            if(e.keyCode === 13) {
+                findMatches();
+            }
+        });
+        $('#reset').click(reset);
+        
+        reset();
     };
 
     return {
